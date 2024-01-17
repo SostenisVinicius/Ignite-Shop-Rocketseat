2775d06cb66d95f5dc69e0b1bf853492d32af231
Sóstenis Vinícius <Socratis__@hotmail.com>
2024-01-16 22:15:04 -0300

    Creating some pages and add stitches styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$sostenish',
+  borderRadius: 8,
+})
+
+export default function Home() {
+  return (
+    <Button>Enviar</Button>
+  )
+}
