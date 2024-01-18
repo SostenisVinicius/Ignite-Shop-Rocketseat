309a1747b01f0b6c3bd70213aeb16492bd36c3e0
Sóstenis Vinícius <Socratis__@hotmail.com>
2024-01-18 17:01:36 -0300

    Create visual structure of product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente voluptatum explicabo omnis? Deleniti quia molestiae magnam maxime repellendus reiciendis non dolorum, neque voluptates, temporibus facere sequi assumenda, enim repellat veniam.</p>
+      
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
